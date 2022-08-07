3808c5be2dafb895af0f6aea668e805755804d91
liuxiaohui <1819800062@qq.com>
2022-08-08 01:07:56 +0800

    脚本

--- models/ECSpell/script.sh
+++ models/ECSpell/script.sh
@@ -1,9 +1,9 @@
-export DIR="E:/pycharm_workspace/syntactic-experts/models/ECSpell"
+export DIR="/root/syntactic-experts/models/ECSpell"
 export MODEL_NAME=glyce
 export FONT_TYPE=sim
-export CHECKPOINT="E:/pycharm_workspace/syntactic-experts/models/ECSpell/Code/Results/ecspell"
+export CHECKPOINT="/root/syntactic-experts/models/ECSpell/Code/Results/ecspell"
 
-python $DIR/Code/train_baseline.py \
+CUDA_VISIBLE_DEVICES=0 python $DIR/Code/train_baseline.py \
 	--model_name $DIR/Transformers/${MODEL_NAME} \
 	--train_files $DIR/Data/traintest/preliminary_train_ecspell.train \
 	--val_files $DIR/Data/traintest/preliminary_val_ecspell.test \
@@ -14,15 +14,15 @@ python $DIR/Code/train_baseline.py \
 	--vocab_file $DIR/Data/vocab/allNoun.txt \
 	--load_pretrain_checkpoint ${CHECKPOINT} \
 	--overwrite_cached True \
-	--num_train_epochs 5 \
+	--num_train_epochs 3 \
 	--gradient_accumulation_steps 2 \
 	--use_pinyin True \
 	--use_word_feature False \
 	--use_copy_label False \
 	--compute_metrics True \
-	--per_device_train_batch_size 2 \
-	--per_device_eval_batch_size 2 \
-	--save_steps 500 \
-	--logging_steps 500 \
+	--per_device_train_batch_size 32 \
+	--per_device_eval_batch_size 32 \
+	--save_steps 2000 \
+	--logging_steps 2000 \
 	--fp16 True \
 	--do_test False \
