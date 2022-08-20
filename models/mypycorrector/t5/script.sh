d337532780b7f101d0cae92ec6dae7ab14d247f0
liuxiaohui <1819800062@qq.com>
2022-08-20 16:43:47 +0800

    t5模型训漏字样本

--- models/mypycorrector/t5/script.sh
+++ models/mypycorrector/t5/script.sh
@@ -2,8 +2,8 @@ export DIR="/root/syntactic-experts/models/mypycorrector/t5"
 export MODEL_NAME="mengzi-t5-base-chinese-correction"
 CUDA_VISIBLE_DEVICES=0 python $DIR/train.py \
 	--model_name_or_path $DIR/pretrained/${MODEL_NAME} \
-	--train_files $DIR/Data//final_train.json \
-	--test_files $DIR/Data/preliminary_val.test \
+	--train_path $DIR/Data/final_train.json \
+	--test_path $DIR/Data/preliminary_val.test \
 	--batch_size 32 \
 	--epochs 10 \
 	--save_dir $DIR/output/mengzi-t5-base-chinese-correction \
